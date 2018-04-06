38884706fb9f65ea1d9554d6c7186f70f59d47ab
sujoydatta26 <sujoydatta26@gmail.com>
2018-04-06 05:31:38 +0530

    Final Project. Doesnt work on Android M+

--- app/src/main/java/com/example/morningstar/silentmodetoggle/MainActivity.java
+++ app/src/main/java/com/example/morningstar/silentmodetoggle/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.morningstar.silentmodetoggle;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.AudioManager;
+import android.view.ContextMenu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import static android.os.Build.VERSION.SDK;
+import static android.os.Build.VERSION.SDK_INT;
 
 public class MainActivity extends AppCompatActivity {
 
     private  AudioManager audio;
     private boolean phoneSilent;
-
+    int bV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //bV = Build.VERSION>SDK_INT;
         audio = (AudioManager)getSystemService(AUDIO_SERVICE);
+        /*try {
+            if (bV >= 23) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                startActivity(intent);
+            }
+        }
+        catch (SecurityException e) {
+        }*/
         isMyPhoneSilent();
         setButtonOnClickListener();
     }
@@ -30,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         btnToggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 if(phoneSilent){
                     //change back to normal mode;
                     audio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
@@ -62,9 +81,9 @@ public class MainActivity extends AppCompatActivity {
         ImageView imgView = (ImageView) findViewById(R.id.imgRingerOn);
         Drawable newPhoneImg;
         if(phoneSilent)
-            newPhoneImg = getResources().getDrawable(R.drawable.ringeroff);
+            newPhoneImg = getResources().getDrawable(R.mipmap.ringeroff);
         else
-            newPhoneImg = getResources().getDrawable(R.drawable.ringeron);
+            newPhoneImg = getResources().getDrawable(R.mipmap.ringeron);
 
         imgView.setImageDrawable(newPhoneImg);
     }
